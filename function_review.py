cea019f11138cb4c4264ec935fba91b130c75699
EC2 Default User <ec2-user@ip-172-31-47-1.us-east-2.compute.internal>
2019-07-18 15:06:38 +0000

    leap year

---
+++ function_review.py
@@ -0,0 +1,12 @@
+# def function_name(here, go, parameter):
+#     internal code
+#     return return_value
+
+#leap year lab
+
+def is_leap_year(year):
+    return (year % 4 == 0)
+    
+print(is_leap_year(1990))
+print(is_leap_year(1991))
+print(is_leap_year(1992))
